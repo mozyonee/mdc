fc49da8a8d4a6c0f308be539a609fff1f72b09db
Chiliad RP <chiliad.forum@gmail.com>
2024-10-27 23:24:04 +0100

    update globals.css, jsconfig.json and middleware.js

--- middleware.js
+++ middleware.js
@@ -1,11 +1,12 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth(
-	middleware = (req) => { },
-	{
-		callbacks: { authorized: ({token}) => !!token },
-		secret: process.env.NEXTAUTH_SECRET,
-	}
-);
+export default withAuth({
+    callbacks: { 
+        authorized: ({ token }) => !!token 
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+});
 
-export const config = { matcher: ["/"] };
+export const config = { 
+    matcher: ["/"]
+};

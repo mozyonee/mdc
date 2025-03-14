f5bae4d13f90aef668ef1dbb4477a8f7f7bc7a2a
Vadym Abakumov <leslieleecoleman@gmail.com>
2025-03-14 18:23:49 +0100

    database environment

--- util/db.js
+++ util/db.js
@@ -2,26 +2,26 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-    connectionLimit: 20,
-    host: "mysql-374c1fbb-mdc-rp.h.aivencloud.com",
-    database: "defaultdb",
-    user: "avnadmin",
-    password: "AVNS_biDYk7NR2j9gi7NNTG1",
-    port: 14756,
-    connectTimeout: 10000
+	connectionLimit: 20,
+	host: process.env.DB_HOST,
+	database: process.env.DB_DATA,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	port: 3306,
+	connectTimeout: 10000
 });
 
 export const query = async (sql, params) => {
-    const connection = await pool.getConnection();
-    try {
-        const [rows] = await connection.query(sql, params);
-        return rows; // Return rows from the query
-    } catch (error) {
-        console.error("Database query error:", error);
-        throw error; // Rethrow error after logging
-    } finally {
-        connection.release(); // Always release connection back to pool
-    }
+	const connection = await pool.getConnection();
+	try {
+		const [rows] = await connection.query(sql, params);
+		return rows;
+	} catch (error) {
+		console.error("Database query error:", error);
+		throw error;
+	} finally {
+		connection.release();
+	}
 };
 
 export default pool;
